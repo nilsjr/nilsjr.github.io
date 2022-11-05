b6b4909c63849867f76370f5117c907059038161
Nils Druyen <info@nilsdruyen.de>
2022-11-05 18:28:11 +0100

    Update 2022.11.1 (#3)
    * Migrate to single page website
    
    * Feature/redesign page (#1)
    
    * feat: redesign webpage WIP
    
    * feat: Implement footer with links WIP
    
    * feat: Finalize Footer.kt
    
    * feat: Redesign timeline & try to fix navigation
    
    * feat: Create data class & cleanup components
    
    Skills WIP
    
    * feat: Implement final design
    
    * chore: Update gradle
    
    * chore: Update workflow
    
    * chore: Add detekt build step
    
    * chore: Update workflow
    
    * chore: Make gradlew executable
    
    * fix: Remove obsolete files
    
    * chore: Update workflows
    
    * chore: Update workflow names
    
    * feat: Update npm versions due dependabot alerts
    
    * chore: Remove old yarn lock file
    
    * Update README.md
    
    * Update README.md
    
    * Update dependencies
    
    * Add loader-utils version

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# nilsjr portfolio
+# NilsJR portfolio
 
 This is my portfolio webpage completely written in kotlin. Its also a showcase to demonstrate creating a static webpages
 with kotlin compose.
 
-tbd.
+# Website
+[Portfolio](https://nilsjr.github.io/)
+
+# Roadmap
+- add more animations
+- hover effects
+- create darkmode
+- update readme
+
+# inspiration
+
+https://github.com/c5inco/portfolio-compose
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
-import org.jetbrains.compose.compose
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 @Suppress("DSL_SCOPE_VIOLATION")
@@ -11,7 +13,7 @@ plugins {
 }
 
 group = "de.nilsdruyen"
-version = "0.0.1"
+version = "0.0.2"
 
 kotlin {
   js(IR) {
@@ -28,6 +30,10 @@ kotlin {
   }
 }
 
+compose {
+  kotlinCompilerPlugin.set("1.3.2.1")
+}
+
 tasks.withType<KotlinCompile>().configureEach {
   kotlinOptions {
     val arguments = listOf(
@@ -39,23 +45,27 @@ tasks.withType<KotlinCompile>().configureEach {
   }
 }
 
-//rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
-//  rootProject.the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().apply {
-//    lockFileDirectory = project.rootDir.resolve(".kotlin-js-store")
-//    resolution("got", "12.1.0")
-//  }
-//  rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().apply {
-//    versions.webpackDevServer.version = "4.9.3"
-//    versions.webpack.version = "5.73.0"
-//    versions.webpackCli.version = "4.10.0"
-//    versions.karma.version = "6.4.0"
-//    versions.mocha.version = "10.0.0"
-//  }
-//}
+rootProject.plugins.withType<YarnPlugin> {
+  rootProject.the<YarnRootExtension>().apply {
+    lockFileDirectory = project.rootDir.resolve(".kotlin-js-store")
+    resolution("async", "2.6.4")
+    resolution("minimist", "1.2.6")
+    resolution("eventsource", "1.1.1")
+    resolution("url-parse", "1.5.8")
+    resolution("loader-utils", "2.0.3")
+  }
+  rootProject.the<NodeJsRootExtension>().apply {
+    versions.webpackDevServer.version = "4.9.3"
+    versions.webpack.version = "5.73.0"
+    versions.webpackCli.version = "4.10.0"
+    versions.karma.version = "6.4.0"
+    versions.mocha.version = "10.0.0"
+  }
+}
 
 // configure detekt
 extensions.configure<DetektExtension> {
-  toolVersion = "1.19.0"
+  toolVersion = "1.21.0"
   source = files("src/jsMain/kotlin")
   parallel = true
   config = files("$rootDir/detekt.yml")
@@ -68,14 +78,4 @@ dependencies {
 // configure dependency updates
 tasks.dependencyUpdates.configure {
   gradleReleaseChannel = "current"
-  rejectVersionIf {
-    isNonStable(candidate.version) && !isNonStable(currentVersion)
-  }
-}
-
-fun isNonStable(version: String): Boolean {
-  val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
-  val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-  val isStable = stableKeyword || regex.matches(version)
-  return isStable.not()
 }
